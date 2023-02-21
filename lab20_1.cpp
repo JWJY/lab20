9fa747556756907ff4136895663f14dd75b6c972
JWJY <warisara.jy@gmail.com>
2023-02-21 16:15:11 +0700

    completed

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,19 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){
+    double a1 ,a2 ,b;
+    a1 = min(R1.x+R1.w,R2.x+R2.w)-max(R1.x,R2.x);
+	a2 = min(R1.y,R2.y)-max(R1.y-R1.h,R2.y-R2.h);
+	b = a1*a2;
+	if(b < 0) b = 0;
+	return b;
 
 
+}
 
+int main(){
+Rect R1 = {1,1,5,5};
+Rect R2 = {2,2,5,5};	
+cout << overlap(R1,R2);
 }
